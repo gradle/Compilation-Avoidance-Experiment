7dcad43f0a94f0f03c16eca45467d47ae0f16045
Tom Tresansky <ttresansky@gradle.com>
2023-08-11 13:29:27 -0400

    Compare with 8.1 instead of 8.2.1

--- comparisons/bazel-and-all-gradle.js
+++ comparisons/bazel-and-all-gradle.js
@@ -1,7 +1,7 @@
 // Comparison data from comparisons/bazel-and-all-gradle.csv
 
 var data = [
-    ['Scenario', 'Bazel', 'Gradle 8.0', 'Gradle 8.2.1', 'Gradle 8.3-rc-3'],
-    ['ABI Change', 42038, 13254, 5426, 4213],
-    ['Non-ABI Change', 1640, 2445, 877, 492]
+    ['Scenario', 'Bazel', 'Gradle 8.0', 'Gradle 8.1', 'Gradle 8.3-rc-3'],
+    ['ABI Change', 40563, 13160, 5382, 4245],
+    ['Non-ABI Change', 1835, 2585, 972, 563]
 ];
